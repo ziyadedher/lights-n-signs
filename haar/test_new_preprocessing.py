8f02c83e3d54b53f12fc18f77e438b999cacad4f
Robert Adragna <robert.adragna1381@gmail.com>
2018-09-23 10:25:46 -0400

    Redesigned data structure containing validation results + added dummy model class

--- haar/test_new_preprocessing.py
+++ haar/test_new_preprocessing.py
@@ -2,12 +2,12 @@ import os
 import math
 import copy
 import cv2
-from typing import List
+from typing import List, Dict
 
 from common import config
-from common import model
-from haar import train
-from haar import model
+from common.model import Bounds2D, PredictedObject2D
+from haar.train import Trainer
+from haar.model import *
 from common import preprocess
 
 def get_image_distance(image_1: List[int], image_2: List[int]) -> float:
@@ -19,7 +19,7 @@ def get_image_distance(image_1: List[int], image_2: List[int]) -> float:
 
     sum = 0
     for i in range(0, len(image_1)):
-        sum += (image_2[i] - image_1[i]) ^ 2
+        sum += math.pow(abs(image_2[i] - image_1[i]), 2)
     return float(math.sqrt(sum))
 
 def compute_bb_overlap(detection: List[int], g_truth: List[int]) -> float:
@@ -39,40 +39,38 @@ def compute_bb_overlap(detection: List[int], g_truth: List[int]) -> float:
     area_g_truth = (g_truth[2]-g_truth[0])*(g_truth[3]-g_truth[1])
     return float((overlap_height*overlap_width)/area_g_truth)
 
+class DummyModel(Model):
+    def predict(self, img):
+        return [PredictedObject2D(Bounds2D(0,0,100,100), ["go"])]
+
+
+
 def benchmark_model(dataset_name: str):
     # Setup the model
 
-    dataset = preprocess.Preprocessor.preprocess(dataset_name)  #The Dataset object for some specified name
-    trainer = train.Trainer("trainer", dataset)
-    trainer.setup_training(24, 500, "go")
-    trainer.train(2, 150, 100)
-    model = trainer.generate_model()
+    dataset = preprocess.preprocess.Preprocessor.preprocess(dataset_name)  #The Dataset object for some specified name
+    '''trainer = Trainer("trainer", dataset)
+trainer.setup_training(24, 5000, "go")
+trainer.train(100, 4000, 2000)
+model = trainer.generate_model()   '''
+    model = DummyModel()
 
     # Unpack the images
-
-    true_image_list = test_info["images"]
-    true_classes_list = test_info["classes"]
-    true_annotations_dict = test_info["annotations"]  ##
-
-
-
-    for image_path in dataset.annotations_test.keys():
+    img_accuracy_stats: Dict[str: Dict[int: Dict[str:union(int, List[int, int, int, int])]]] = {}
+    for image_path in dataset.test_annotations:
         # Get model's detections
+        print ("before statement")
         img_file = cv2.imread(image_path)
         detections_list = model.predict(img_file)
+        print("detections_list is okay")
 
-        # Check if right # of predictions
-        if len(detections_list) == len(dataset.annotations_test[image_path]):
-            right_predict_num = True
-        else:
-            right_predict_num = False
 
         # Assume the right number of predictions, map ground truth to prediction
         truth_to_predict_map = [0] * len(detections_list)  #index=index of detection in list, value=believed corresponding true light
-        possible_gt_list = copy.deepcopy(dataset.annotations_test[image_path])
 
+        possible_gt_list = copy.deepcopy(dataset.test_annotations[image_path])
         for ind, detection in enumerate(detections_list):
-            closest = [[0,{}]]  # Initialize list keeps track of closest feature to detection - its distance + values in annotations_list form
+            closest = [0,{}]  # Initialize list keeps track of closest feature to detection - its distance + values in annotations_list form
             removal_index = 0  #Index of possible ground truth that will be removed after each iteration
 
             #Enumerate through each GT detect to find which is most likely the feature of interest
@@ -89,50 +87,31 @@ def benchmark_model(dataset_name: str):
 
             #Set the true BB associated with detection and, if appropiate, delete gorund truth possibilty
             truth_to_predict_map[ind] = closest[1]
-            if len(detections_list) <= len(dataset.annotations_test[image_path]):
+            if len(detections_list) <= len(dataset.test_annotations[image_path]):
                 del possible_gt_list[removal_index]
 
-        #Compute statistics about how accruate predicitons are
-        accuracy_stats = {}  #key=index in detecitons_list. value = dictionary of stat name to value. Stats include
-        #BB overlap, whether class type is correct
-
+        #Create and populate data strucutre containing accuracy stats for each detection
+        img_accuracy_stats[image_path] = {}
         for index, detection in enumerate(detections_list):
-            accuracy_stats[index] = {}
-            accuracy_stats[index]["correct_class"] = (detection.predicted_classes[0] == truth_to_predict_map[index]["class"])
-            accuracy_stats[index]["bounding_box_overlap"] = compute_bb_overlap([detection.bounding_box.left, detection.bounding_box.top,
+            img_accuracy_stats[image_path][index] = {}
+
+            #compute relevant per detection statistics
+            is_type_accurate = (detection.predicted_classes[0] == truth_to_predict_map[index]["class"])
+            bb_overlap = compute_bb_overlap([detection.bounding_box.left, detection.bounding_box.top,
                                    detection.bounding_box.left + detection.bounding_box.width, detection.bounding_box.top + detection.bounding_box.height],
-                                                                              [truth_to_predict_map[index]["x_min"],truth_to_predict_map[index]["y_min"],
-                                                                               truth_to_predict_map[index]["x_max"], truth_to_predict_map[index]["y_max"]])
+                                        [truth_to_predict_map[index]["x_min"],truth_to_predict_map[index]["y_min"],
+                                        truth_to_predict_map[index]["x_max"], truth_to_predict_map[index]["y_max"]])
 
+            #assign relevant per detectionstatistics
+            img_accuracy_stats[image_path][index]["correct_class"] = is_type_accurate
+            img_accuracy_stats[image_path][index]["bounding_box_overlap"] = bb_overlap
 
+        # Create and populate data strucutre containing accuracy stats for entire dataset
+        # average_type_error = float(total_type_error/len(detections_list))
+        # average_bounding_box_overlap = float(total_bounding_box_error/len(detections_list))
+        # right_predict_num = len(detections_list) == len(dataset.annotations_test[image_path])
 
-        # Convert list of detections into same format as ground truth annotations
-        processed_detections_list = []
-        for detection in detections_list:
-            processed_detections_list.append(
-                {"class": detection.predicted_classes, "x_min": detection.bounding_box.left,
-                 "y_min": detection.bounding_box.top,
-                 "x_max": detection.bounding_box.left + detection.bounding_box.width,
-                 "y_max": detection.bounding_box.top + detection.bounding_box.height})
-        detections_list = processed_detections_list
-
-
-        # Assign predictions to real detections
-
-
-
-
-        # Compare accuracy of those detections to ground truth
-        real_test_img_annotations = annotations_dict[image_path]
-        real_class_name = annotations_dict[image_path][0]["class"]
-        real_bounding_box = model.Bounds2D(annotations_dict[image_path][0]["x_min"],
-                                           annotations_dict[image_path][0]["y_max"],
-                                           annotations_dict[image_path][0]["x_max"] - annotations_dict[image_path][0][
-                                               "x_min"],
-                                           annotations_dict[image_path][0]["y_max"] - annotations_dict[image_path][0][
-                                               "y_min"])
-
-
+#confusion matrix
 
 if __name__ == '__main__':
     benchmark_model("LISA")
