20e5fc92b8d15384df5cd56318122fc79be7b7df
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2018-09-23 11:39:08 -0400

    Create barebones SqueezeDet files

---
+++ squeezedet/lns_squeezedet/process.py
@@ -0,0 +1,5 @@
+"""Data processing for SqueezeDet training.
+
+Manages all data processing for the generation of data ready to be trained
+on with SqueezeDet Keras fitting functions.
+"""
---
+++ squeezedet/lns_squeezedet/train.py
@@ -0,0 +1,4 @@
+"""SqueezeDet training script.
+
+This script manages model training and generation.
+"""
