4a9b7bcb550068eac51a9ebaa11827f25b92ad07
Robert Adragna <robert.adragna1381@gmail.com>
2018-10-07 17:36:54 -0400

    Completely implemented confusion matrix

--- haar/test_new_preprocessing.py
+++ haar/test_new_preprocessing.py
@@ -57,10 +57,38 @@ def find_average(full_accuracy_stats, param_name):
 
     return sum_of_param/len(full_accuracy_stats.keys())
 
-def find_confusion_matrix(true_structure, detection_strucutre, true_det_map ):
+def find_confusion_matrix(true_structure, detection_strucutre, det_true_map, dataset):
     '''Take a full test results data structure and divide into true positive, true negatives, etc'''
-    pass
 
+    #Set up matrix structure
+    confusion_matrix : Dict[str: Dict[str:int]] = {}  #every predicted-actual combo is given as a single int in a dicitonary
+    class_names = dataset.classes
+    class_names.append("None")
+    for row_type in class_names :
+        confusion_matrix[row_type] = {}
+        for col_type in class_names :
+            confusion_matrix[row_type][col_type] = 0
+
+    #populate the matrix
+    for image in detection_strucutre.keys() :   #get truths, detections of some image
+        det_list = detection_strucutre[image]
+        true_list = true_structure[image]
+        detected_true_indicies = []  #keeps track of which true indicies map to detections
+
+        for det_ind, detection in enumerate(det_list):  #iterate through detections, populate matrix
+            if det_true_map[image][det_ind] == None :  #handle case where detection without true feature
+                confusion_matrix[detection["class"]]["None"] += 1
+            else:
+                a = dataset.classes[true_list[det_true_map[image][det_ind]]["class"]]
+                confusion_matrix[detection["class"]][dataset.classes[true_list[det_true_map[image][det_ind]]["class"]]] += 1
+            detected_true_indicies.append(det_ind)
+
+        if len(true_list) > len(det_list)  :  #handle case where true feature without detection
+            undetected_true_indicies = [x for x in range(0, len(true_list)) if x not in detected_true_indicies]
+            for index in undetected_true_indicies :
+                confusion_matrix["None"][dataset.classes[index]] += 1
+
+    return confusion_matrix
 
 
 class DummyModel(Model):
@@ -81,16 +109,16 @@ def benchmark_model(dataset_name: str):
     # Unpack the images
     img_accuracy_stats: Dict[str: Dict[int: Dict[str:union(int, List[int, int, int, int])]]] = {}
     detection_annotations: Dict[str: List[Dict[str,int]]] = {}
+    predict_to_truth_map: Dict[str:Dict[int:int]] = {}
     for image_path in dataset.test_annotations:
         # Get model's detections
         img_file = cv2.imread(image_path)
         detections_list = model.predict(img_file)
 
         # Go through all detections, map each to the closest ground truth detectable object
-        predict_to_truth_map = {}
+        predict_to_truth_map[image_path] = {}
         possible_gt_list = copy.deepcopy(dataset.test_annotations[image_path])
         removal_indicies = []  # allows accurate transform to g_truth indicides
-
 
         for ind, detection in enumerate(detections_list):
             if len(possible_gt_list) > 0:
@@ -118,10 +146,10 @@ def benchmark_model(dataset_name: str):
                             true_index += 1
                     true_index = closest[1] + true_index
 
-                predict_to_truth_map[ind] = true_index
+                predict_to_truth_map[image_path][ind] = true_index
 
             else:
-                predict_to_truth_map[ind] = None
+                predict_to_truth_map[image_path][ind] = None
 
 
         #Populate data strucutre containing predicted features on each image. Equivilant to test_annotations structure
@@ -130,13 +158,14 @@ def benchmark_model(dataset_name: str):
             detection_annotations[image_path].append({})
 
             detection_annotations[image_path][index]["class"] = detection.predicted_classes[0]
-            detection_annotations[image_path][index]["x_min"] = detection.bounding_box[0]
-            detection_annotations[image_path][index]["x_max"] = detection.bounding_box[1]
-            detection_annotations[image_path][index]["y_min"] = detection.bounding_box[2]
-            detection_annotations[image_path][index]["y_max"] = detection.bounding_box[3]
+            detection_annotations[image_path][index]["x_min"] = detection.bounding_box.left
+            detection_annotations[image_path][index]["x_max"] = detection.bounding_box.left + detection.bounding_box.width
+            detection_annotations[image_path][index]["y_min"] = detection.bounding_box.top
+            detection_annotations[image_path][index]["y_max"] = detection.bounding_box.top + detection.bounding_box.height
 
     #Generate and print all accuracy statistics
-    find_confusion_matrix(dataset.test_annotations, detection_annotations, predict_to_truth_map)
+    confusion_matrix = find_confusion_matrix(dataset.test_annotations, detection_annotations, predict_to_truth_map, dataset)
+    print(confusion_matrix)
 
 if __name__ == '__main__':
     benchmark_model("LISA")
