d450857350d1f4be2fc39c7fea1a55d9d20cde6c
Quinbit <quinsykora@gmail.com>
2018-11-04 12:06:57 -0500

    Changed make_model to output folder called yolo_model, other models may use it too
    Former-commit-id: 8cc153e385c9e6f693873657b930b020d7ccc56c

--- yolov3/make_model.sh
+++ yolov3/make_model.sh
@@ -1,13 +1,13 @@
-mkdir model
-cp -r yolo3 model
-cp yolo.py model
-mkdir model/logs
-mkdir model/logs/000/
+mkdir yolo_model
+cp -r yolo3 yolo_model
+cp yolo.py yolo_model
+mkdir yolo_model/logs
+mkdir yolo_model/logs/000/
 cp logs/000/trained_weights_final.h5 model/logs/000/trained_weights_final.h5
-cp classes.txt model
-mkdir model/model_data
-cp model_data/yolo_anchors.txt model/model_data/yolo_anchors.txt
-mkdir model/lns_common
-cp lns_common/model.py model/lns_common/model.py
-touch model/__init__.py
-cp requirements.txt model/requirements.txt
+cp classes.txt yolo_model
+mkdir yolo_model/model_data
+cp model_data/yolo_anchors.txt yolo_model/model_data/yolo_anchors.txt
+mkdir yolo_model/lns_common
+cp lns_common/model.py yolo_model/lns_common/model.py
+touch yolo_model/__init__.py
+cp requirements.txt yolo_model/requirements.txt
