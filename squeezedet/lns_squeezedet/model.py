e10118f3801b1b4391cb3b155987578aab5161e5
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2018-11-09 10:24:54 -0500

    Fix incorrect bounding box scaling in SqueezeDet model
    Former-commit-id: 1f95d58f6051908d94c77b0cb5c6fe2c25820c1b

--- squeezedet/lns_squeezedet/model.py
+++ squeezedet/lns_squeezedet/model.py
@@ -30,6 +30,9 @@ class SqueezeDetModel(Model):
 
     def predict(self, image: np.ndarray) -> List[PredictedObject2D]:
         """Predict the required bounding boxes on the given <image>."""
+        # Store the original width and height before resizing
+        width, height, _ = image.shape
+
         # Preprocess the image how squeezedet_keras does it
         image = cv2.resize(
             image, (self.__config.IMAGE_WIDTH, self.__config.IMAGE_HEIGHT)
@@ -52,6 +55,12 @@ class SqueezeDetModel(Model):
         boxes = boxes[0]
         classes = classes[0]
         scores = scores[0]
+
+        # Resize the bounding boxes back to original size
+        boxes[0] *= width / self.__config.IMAGE_WIDTH
+        boxes[1] *= height / self.__config.IMAGE_HEIGHT
+        boxes[2] *= width / self.__config.IMAGE_WIDTH
+        boxes[3] *= height / self.__config.IMAGE_HEIGHT
 
         # Generate the prediction boxes
         return [
