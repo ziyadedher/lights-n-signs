af5e2b766c9b76d170be7f9bc1cf92bba9688d06
Quinbit <quinsykora@gmail.com>
2019-02-16 20:25:11 +0000

    Update lns_common/preprocess/preprocessing.py
    Former-commit-id: ebe37ffc66358ebfea895fb869df860877e90ac3

--- lns_common/preprocess/preprocessing.py
+++ lns_common/preprocess/preprocessing.py
@@ -204,6 +204,112 @@ def preprocess_LISA(LISA_path: str, proportion: float = 1.0,
         return create_testset("cities", {"cities": images},
                               detection_classes, annotations, proportion)
 
+def preprocess_lights(lights_path: str, proportion: float = 1.0,
+                      testset: bool = False) -> Dataset:
+    """Preprocess and generate data for our custom dataset at the given path.
+
+    Raises `FileNotFoundError` if any of the required files or folders is not
+    found.
+    """
+    images: List[str] = []
+    detection_classes: List[str] = []
+    annotations: Dict[str, List[Dict[str, int]]] = {}
+
+    if not os.path.isdir(os.path.join(lights_path, "images_new")):
+        os.mkdir(os.path.join(
+            lights_path, "images_new"
+        ))
+
+    annotation_files: List[str] = os.listdir(lights_path)
+
+    if len(annotation_files) == 0:
+        raise FileNotFoundError(
+            f"Could not find annotations file {directory}."
+        )
+
+    files_created: List[str] = os.listdir(os.path.join(
+        lights_path, "images_new"
+    ))
+
+    for f in annotation_files:
+        if os.path.isdir(os.path.join(lights_path, f)):
+            continue
+
+        print("Processing files for {}".format(f))
+
+        with open(os.path.join(lights_path, f)) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            id_index = 0
+            annos = 0
+
+            for row in csv_reader:
+                if id_index == 0:
+                    for r in range(len(row)):
+                        if "Answer.annotatedResult.boundingBoxes" in row[r]:
+                            annos = r
+                        if "Input.image_id" in row[r]:
+                            id_index = r
+                else:
+                    img_id = str(row[id_index])
+                    url = "https://drive.google.com/uc?id={}&export=download".f$
+                    print(url)
+                    if "{}.png".format(img_id) not in files_created:
+                        urllib.request.urlretrieve(
+                            url, "{}.png".format(os.path.join(
+                                lights_path, 'images_new', img_id
+                            ))
+                        )
+                        print("{}.png Downloaded".format(img_id))
+
+                        files_created.append("{}.png".format(img_id))
+
+                    image_path = os.path.abspath(
+                        os.path.join(
+                            lights_path, "images_new", "{}.png".format(img_id)
+                        )
+                    )
+
+                    data = json.loads(row[annos])
+
+                    if image_path not in annotations:
+                        annotations[image_path] = []
+
+                    for d in data:
+                        c = d["label"]
+
+                        if c not in detection_classes:
+                            detection_classes.append(c)
+
+                        x = d["left"]
+                        y = d["top"]
+                        w = d["width"]
+                        h = d["height"]
+
+                        if config.MIN_SIZE > w * h:
+                            continue
+
+                        annotations[image_path].append({
+                            "class": detection_classes.index(c),
+                            "x_min": x,
+                            "y_min": y,
+                            "x_max": x + w,
+                            "y_max": y + h
+                        })
+
+                    images.append(image_path)
+
+    annotations, images = delete_empties(annotations, images)
+
+    annotations = annotation_fix(annotations)
+
+    if not testset:
+        return set_proportions("lights", {"lights": images},
+                               detection_classes, annotations, proportion)
+    else:
+        return create_testset("lights", {"lights": images},
+                              detection_classes, annotations, proportion)
+
+
 
 def preprocess_sim(sim_path: str, proportion: float = 1.0,
                    testset: bool = False) -> Dataset:
