9b27a581ecfe4a4385a25fba51d29f9673032afb
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-09-15 08:11:38 -0400

    Introduce model functionality to predict on an image path

--- lns/common/model.py
+++ lns/common/model.py
@@ -5,6 +5,7 @@ common interface for every technique for light and sign detection.
 """
 from typing import TypeVar, List
 
+import cv2          # type: ignore
 import numpy as np  # type: ignore
 
 from lns.common.structs import Object2D
@@ -16,6 +17,10 @@ class Model:  # noqa: R903
     def predict(self, image: np.ndarray) -> List[Object2D]:
         """Predict objects on the given <image> using this model."""
         raise NotImplementedError
+
+    def predict_path(self, path: str) -> List[Object2D]:
+        """Predicts objects on the image at the given <path> using this model."""
+        return self.predict(cv2.imread(path))
 
 
 ModelType = TypeVar("ModelType", bound=Model)
