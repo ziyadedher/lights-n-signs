88199da8db05d85b75b24b51db4778ed8dc77ea6
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-09-15 08:43:10 -0400

    Allow visualization of predictions and ground truths

--- lns/common/visualization.py
+++ lns/common/visualization.py
@@ -1,13 +1,15 @@
 """"""
 
-from typing import Optional, Union
+from typing import Tuple, List, Optional, Union
 
 import random
 
-import cv2  # type: ignore
+import cv2          # type: ignore
+import numpy as np  # type: ignore
 
 from lns.common.model import Model
 from lns.common.dataset import Dataset
+from lns.common.structs import Object2D
 from lns.common.preprocess import Preprocessor
 
 
@@ -15,16 +17,13 @@ QUIT_KEY = "q"
 
 
 def visualize(dataset: Union[Dataset, str], model: Optional[Model] = None, *,
-              shuffle: bool = False) -> None:
+              shuffle: bool = False, show_truth: bool = True) -> None:
     """Visualize the given <dataset>.
 
     If a <model> is supplied, visualizes with predictions from that model.
     If <shuffle> is set to `True`, randomly shuffles the images in the
     dataset before visualizing.
     """
-    thickness = 1
-    color = (255, 255, 255)
-
     if isinstance(dataset, str):
         dataset = Preprocessor.preprocess(dataset)
     window_name = f"visualization_{dataset.name}"
@@ -38,14 +37,23 @@ def visualize(dataset: Union[Dataset, str], model: Optional[Model] = None, *,
         image = cv2.imread(image_path)
 
         if model:
+            predictions = model.predict(image)
+            draw_labels(image, predictions, (255, 0, 0), 1)
+
+        if show_truth:
             labels = annotations[image_path]
-            for label in labels:
-                cv2.rectangle(
-                    image,
-                    (label.bounds.left, label.bounds.top), (label.bounds.right, label.bounds.bottom),
-                    color=color, thickness=thickness
-                )
+            draw_labels(image, labels, (255, 255, 255), 2)
 
         cv2.imshow(window_name, image)
         if cv2.waitKey() == ord(QUIT_KEY):
             break
+
+
+def draw_labels(image: np.ndarray, labels: List[Object2D], color: Tuple[int, int, int], thickness: int) -> None:
+    """Draw the given <labels> on the given <image>."""
+    for label in labels:
+        cv2.rectangle(
+            image,
+            (label.bounds.left, label.bounds.top), (label.bounds.right, label.bounds.bottom),
+            color=color, thickness=thickness
+        )
