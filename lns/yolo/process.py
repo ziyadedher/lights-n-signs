c6887c67825d5de0d62fde6bf96981ebad39982d
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-10-01 00:19:52 -0400

    Introduce YOLO processor

---
+++ lns/yolo/process.py
@@ -0,0 +1,80 @@
+"""Data processing for YOLOv3.
+
+Manages all data processing for the generation of data ready to be trained on with our YOLOv3 training backend.
+"""
+from typing import ClassVar, Iterable, Tuple
+
+import os
+
+from tqdm import tqdm  # type: ignore
+
+from lns.common.dataset import Dataset
+from lns.common.process import ProcessedData, Processor
+
+
+class YoloData(ProcessedData):
+    """Data container for all YOLOv3 processed data.
+
+    Stores paths to all files needed by backend YOLOv3 training.
+    """
+
+    __classes: str
+    __annotations: str
+
+    def __init__(self, classes: str, annotations: str) -> None:
+        """Initialize the data structure."""
+        self.__classes = classes
+        self.__annotations = annotations
+
+    def get_classes(self) -> str:
+        """Get the path to the class names file."""
+        return self.__classes
+
+    def get_annotations(self) -> str:
+        """Get the path to the annotations file."""
+        return self.__annotations
+
+
+class YoloProcessor(Processor[YoloData]):
+    """YOLOv3 processor responsible for data processing to YOLOv3-valid formats."""
+
+    METHOD: ClassVar[str] = "yolo"
+
+    @classmethod
+    def method(cls) -> str:
+        """Get the training method this processor is for."""
+        return cls.METHOD
+
+    @classmethod
+    def _process(cls, dataset: Dataset) -> YoloData:
+        processed_data_folder = os.path.join(cls.get_processed_data_path(), dataset.name)
+
+        classes = dataset.classes
+        classes_path = os.path.join(processed_data_folder, "classes")
+        with open(classes_path, "w") as classes_file:
+            def generate_classes() -> Iterable[str]:
+                for class_name in classes:
+                    yield f"{class_name}\n"
+
+            classes_file.writelines(generate_classes())
+
+        images = dataset.images
+        annotations = dataset.annotations
+        annotations_path = os.path.join(processed_data_folder, "annotations")
+        with open(annotations_path, "w") as annotations_file:
+            def order_label(label) -> Tuple[str, str, str, str, str]:
+                return (str(label.bounds.left), str(label.bounds.top),
+                        str(label.bounds.right), str(label.bounds.bottom), str(label.class_index))
+
+            def generate_annotations() -> Iterable[str]:
+                for image in tqdm(images, desc=f"YOLO Processing `{dataset.name}`"):
+                    labels = annotations[image]
+                    labels_str = " ".join(",".join(order_label(label)) for label in labels)
+                    yield f"{image} {labels_str}\n"
+
+            annotations_file.writelines(generate_annotations())
+
+        return YoloData(classes_path, annotations_path)
+
+
+YoloProcessor.init_cached_processed_data()
