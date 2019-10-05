7a07092c75950cc461c9076299620729c645b42f
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-10-05 19:31:31 -0400

    Speed up imports

--- lns/yolo/model.py
+++ lns/yolo/model.py
@@ -11,10 +11,6 @@ from lns.common.model import Model
 from lns.common.structs import Object2D
 from lns.yolo.settings import YoloSettings
 
-from lns.yolo._lib.utils.eval_utils import get_preds_gpu
-from lns.yolo._lib.utils.nms_utils import gpu_nms
-from lns.yolo._lib.model import yolov3
-
 
 class YoloModel(Model):
     """Detection model utilizing YOLOv3."""
@@ -24,7 +20,11 @@ class YoloModel(Model):
         if not settings:
             settings = YoloSettings()
 
+        from lns.yolo._lib.utils.nms_utils import gpu_nms
+        from lns.yolo._lib.model import yolov3
         from lns.yolo._lib import args
+
+        args.restore_path = weights_file
         for field, setting in zip(settings._fields, settings):
             setattr(args, field, setting)
         args.optimizer_name = settings.optimizer_name.value
@@ -49,6 +49,8 @@ class YoloModel(Model):
 
     def predict(self, image: np.ndarray) -> List[Object2D]:
         """Predict the required bounding boxes on the given <image>."""
+        from lns.yolo._lib.utils.eval_utils import get_preds_gpu
+
         y_pred = self._session.run([self._y_pred], feed_dict={self._is_training: False, self._image: image})
         pred_content = get_preds_gpu(self._session, self._gpu_nms_op, self._pred_boxes_flag,
                                      self._pred_scores_flag, [0], y_pred)
