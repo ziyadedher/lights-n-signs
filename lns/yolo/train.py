caa84e9405312f7506c076852483df137c603963
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-10-05 14:47:55 -0400

    Get the string value from the enum

--- lns/yolo/train.py
+++ lns/yolo/train.py
@@ -176,6 +176,8 @@ class YoloTrainer(Trainer[YoloModel, YoloData]):
 
         for field, setting in zip(settings._fields, settings):
             setattr(args, field, setting)
+        args.optimizer_name = args.optimizer_name.value
+        args.lr_type = args.lr_type.value
 
         args.init()
 
