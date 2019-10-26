dfd6db6309fea398c13175995d0f495102c251b6
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-10-26 14:44:48 -0400

    Increase visibility of key trainer classes to main modules

--- lns/haar/__init__.py
+++ lns/haar/__init__.py
@@ -3,3 +3,10 @@
 This packages contains all processes, training, and models relating to
 Haar cascade training and development.
 """
+
+from lns.haar.model import HaarModel
+from lns.haar.process import HaarProcessor
+from lns.haar.settings import HaarSettings
+from lns.haar.train import HaarTrainer
+
+__all__ = ["HaarModel", "HaarProcessor", "HaarSettings", "HaarTrainer"]
