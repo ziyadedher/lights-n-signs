66b6dd1e36f9266dd85394cd2489a680babe917c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-11-02 16:46:40 -0400

    get img dims without reading whole file

--- lns/common/dataset_distance.py
+++ lns/common/dataset_distance.py
@@ -8,7 +8,8 @@ from typing import List, Tuple
 
 from lns.common.dataset import Dataset
 
-from PIL import Image
+import os
+import struct
 
 def distance(dataset: Dataset) -> Dict[str, List[Tuple[int, float]]]:
     """Return a mapping of images to a list of object distances.
@@ -26,7 +27,70 @@ def distance(dataset: Dataset) -> Dict[str, List[Tuple[int, float]]]:
 def _img_area(img_name: str) -> float:
     """Determine dimensions of image stored at absolute path <img_name>.
     """
-    # TODO: compute this without loading in the whole image?
-    im = Image.open(img_name)
-    width, height = im.size
+    width, height = get_image_size(img_name)
     return width * height
+
+# https://stackoverflow.com/questions/15800704/get-image-size-without-loading-image-into-memory
+# https://raw.githubusercontent.com/scardine/image_size/master/get_image_size.py
+class UnknownImageFormat(Exception):
+    pass
+
+def get_image_size(file_path):
+    """
+    Return (width, height) for a given img file content - no external
+    dependencies except the os and struct modules from core
+    """
+    size = os.path.getsize(file_path)
+
+    with open(file_path) as input:
+        height = -1
+        width = -1
+        data = input.read(25)
+
+        if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+            # GIFs
+            w, h = struct.unpack("<HH", data[6:10])
+            width = int(w)
+            height = int(h)
+        elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
+              and (data[12:16] == 'IHDR')):
+            # PNGs
+            w, h = struct.unpack(">LL", data[16:24])
+            width = int(w)
+            height = int(h)
+        elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+            # older PNGs?
+            w, h = struct.unpack(">LL", data[8:16])
+            width = int(w)
+            height = int(h)
+        elif (size >= 2) and data.startswith('\377\330'):
+            # JPEG
+            msg = " raised while trying to decode as JPEG."
+            input.seek(0)
+            input.read(2)
+            b = input.read(1)
+            try:
+                while (b and ord(b) != 0xDA):
+                    while (ord(b) != 0xFF): b = input.read(1)
+                    while (ord(b) == 0xFF): b = input.read(1)
+                    if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                        input.read(3)
+                        h, w = struct.unpack(">HH", input.read(4))
+                        break
+                    else:
+                        input.read(int(struct.unpack(">H", input.read(2))[0])-2)
+                    b = input.read(1)
+                width = int(w)
+                height = int(h)
+            except struct.error:
+                raise UnknownImageFormat("StructError" + msg)
+            except ValueError:
+                raise UnknownImageFormat("ValueError" + msg)
+            except Exception as e:
+                raise UnknownImageFormat(e.__class__.__name__ + msg)
+        else:
+            raise UnknownImageFormat(
+                "Sorry, don't know how to get information from this file."
+            )
+
+    return width, height
