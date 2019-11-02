e39193bdc82fc487c500b994de8369c120bb212b
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-11-02 12:25:33 -0400

    Implement Squeezedet dataset processing

--- lns/squeezedet/process.py
+++ lns/squeezedet/process.py
@@ -3,38 +3,38 @@
 Manages all data processing for the generation of data ready to be trained
 on with SqueezeDet Keras fitting functions.
 """
-from typing import List
-
 import os
+from typing import List
 
 from lns.common.dataset import Dataset
 from lns.common.process import ProcessedData, Processor
 
-class SqueezeDetData(ProcessedData):
+
+class SqueezedetData(ProcessedData):
     """Data container for all SqueezeDet processed data.
 
     Store paths to all files needed by backend SqueezeDet training.
     """
 
-    __images: List[str]
-    __labels: List[str]
+    __images: str
+    __labels: str
 
-    def __init__(self, images: List[str], labels: List[str]) -> None:
+    def __init__(self, images: str, labels: str) -> None:
         """Initialize the data structure."""
         self.__images = images
         self.__labels = labels
 
-    def get_images(self) -> List[str]:
-        """Get a list of paths to all images."""
+    def get_images(self) -> str:
+        """Get a path to the text file containing paths to all images."""
         return self.__images
 
-    def get_labels(self) -> List[str]:
-        """Get a list of paths to all ground truths."""
+    def get_labels(self) -> str:
+        """Get a path to the text file containing paths to all labels."""
         return self.__labels
 
 
-class SqueezeDetProcessor(Processor[SqueezeDetData]):
-    """SqueezeDet processor responsible for data processing to SqueezeDet-valid formats."""
+class SqueezedetProcessor(Processor[SqueezedetData]):
+    """Squeezedet processor responsible for data processing to Squeezedet-valid formats."""
 
     @classmethod
     def method(cls) -> str:
@@ -42,12 +42,7 @@ class SqueezeDetProcessor(Processor[SqueezeDetData]):
         return "squeezedet"
 
     @classmethod
-    def _process(cls, dataset: Dataset) -> SqueezeDetData:
-        """Process all required data from the dataset with the given name.
-
-        Raises `NoPreprocessorException` if a preprocessor for the dataset does
-        not exist.
-        """
+    def _process(cls, dataset: Dataset) -> SqueezedetData:  # pylint: disable=too-many-locals
         # Register all folders
         processed_data_folder = os.path.join(cls.get_processed_data_path(), dataset.name)
         labels_folder = os.path.join(processed_data_folder, "labels")
@@ -63,8 +58,8 @@ class SqueezeDetProcessor(Processor[SqueezeDetData]):
             label_strings: List[str] = []
             for annotation in annotations:
                 if (annotation.bounds.left < 0 or annotation.bounds.top < 0
-                    or annotation.bounds.left > annotation.bounds.right
-                    or annotation.bounds.top > annotation.bounds.bottom):
+                   or annotation.bounds.left > annotation.bounds.right
+                   or annotation.bounds.top > annotation.bounds.bottom):
                     continue
 
                 # NOTE: see https://github.com/NVIDIA/DIGITS/issues/992
@@ -75,10 +70,10 @@ class SqueezeDetProcessor(Processor[SqueezeDetData]):
                     "0",                                   # truncation number
                     "0",                                   # occlusion number
                     "0",                                   # observation angle
-                    str(annotation.bounds.left),              # left
-                    str(annotation.bounds.top),              # top
-                    str(annotation.bounds.right),              # right
-                    str(annotation.bounds.bottom),              # bottom
+                    str(annotation.bounds.left),           # left
+                    str(annotation.bounds.top),            # top
+                    str(annotation.bounds.right),          # right
+                    str(annotation.bounds.bottom),         # bottom
                     "0",                                   # height (3d)
                     "0",                                   # width  (3d)
                     "0",                                   # length (3d)
@@ -90,7 +85,7 @@ class SqueezeDetProcessor(Processor[SqueezeDetData]):
                 )))
 
             # Do not write empty files
-            if len(label_strings) == 0:
+            if not label_strings:
                 continue
 
             images.append(image)
@@ -114,6 +109,7 @@ class SqueezeDetProcessor(Processor[SqueezeDetData]):
         with open(images_path, "w") as images_file:
             images_file.write("\n".join(images))
 
-        return SqueezeDetData(images, labels)
+        return SqueezedetData(labels_path, images_path)
 
-SqueezeDetProcessor.init_cached_processed_data()
+
+SqueezedetProcessor.init_cached_processed_data()
