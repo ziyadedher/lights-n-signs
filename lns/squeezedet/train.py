a97f9b4d5c87ea966fdee20b0f1ac63291c13a7b
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-11-02 12:26:02 -0400

    Implement Squeezedet training

--- lns/squeezedet/train.py
+++ lns/squeezedet/train.py
@@ -1,114 +1,110 @@
-"""SqueezeDet training script.
+"""Squeezedet trainer.
 
-This script manages model training and generation.
+The module manages the representation of a Squeezedet training session along with all associated data.
 """
-from typing import Union, Optional, List
-
+import dataclasses
 import os
-import pickle
-import inspect
-import easydict                     
-import numpy as np     
+from typing import Optional, Union
 
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras import optimizers
-
-from lns.common.train import Trainer
+from lns.common import config
 from lns.common.dataset import Dataset
-from lns.squeezedet.model import SqueezeDetModel
-from lns.squeezedet.process import SqueezeDetData, SqueezeDetProcessor
-from lns.squeezedet.lib import SqueezeDet
+from lns.common.train import Trainer
+from lns.squeezedet.model import SqueezedetModel
+from lns.squeezedet.process import SqueezedetData, SqueezedetProcessor
+from lns.squeezedet.settings import SqueezedetSettings
 
-class SqueezeDetTrainer(Trainer[SqueezeDetModel, SqueezeDetData]):
-    """Manages the SqueezeDet training environment.
+
+class SqueezedetTrainer(Trainer[SqueezedetModel, SqueezedetData, SqueezedetSettings]):
+    """Manages the Squeezedet training environment and execution.
 
     Contains and encapsulates all training setup and files under one namespace.
     """
 
-    _WEIGHTS_INIT_FILE = os.path.join(
-        os.path.dirname(__file__), "imagenet.h5"
-    )
-
-    _squeeze: SqueezeDet
-    _load: bool
-
     SUBPATHS = {
-        "checkpoint_folder": Trainer.Subpath(
-            path="checkpoint", temporal=True, required=True, path_type=Trainer.PathType.FOLDER),
-        "tensorboard_folder": Trainer.Subpath(
-            path="tensorboard", temporal=True, required=True, path_type=Trainer.PathType.FOLDER),
+        "log_folder": Trainer.Subpath(
+            path="log", temporal=True, required=True, path_type=Trainer.PathType.FOLDER),
+        "anchors_file": Trainer.Subpath(
+            path="anchors", temporal=False, required=False, path_type=Trainer.PathType.FILE),
         "config_file": Trainer.Subpath(
             path="config", temporal=True, required=False, path_type=Trainer.PathType.FILE),
     }
 
-    def __init__(self, name: str,
-                 dataset: Union[str, Dataset],
-                 load: bool = True) -> None:
-        """Initialize a SqueezeDet trainer with the given unique <name>.
+    INITIAL_WEIGHTS_NAME = "imagenet.hd5"
+    INITIAL_WEIGHTS = os.path.join(config.RESOURCES_ROOT, config.WEIGHTS_FOLDER_NAME, INITIAL_WEIGHTS_NAME)
 
-        Sources data from the <dataset> given which can either be a name of
-        an available dataset or a `Dataset` object. If <load> is set
-        to `False` removes any existing trained checkpoints before training.
+    settings: SqueezedetSettings
+
+    def __init__(self, name: str, dataset: Optional[Union[str, Dataset]] = None, load: bool = True) -> None:
+        """Initialize a Squeezedet trainer with the given unique <name>.
+
+        Sources data from the <dataset> given, if any.
+        If <load> is set to False removes any existing training files before training.
         """
         super().__init__(name, dataset,
-                         _processor=SqueezeDetProcessor, _method="squeezedet",
-                         _load=load, _subpaths=self.SUBPATHS)
+                         _processor=SqueezedetProcessor, _settings=SqueezedetSettings,
+                         _load=load, _subpaths=SqueezedetTrainer.SUBPATHS)
 
-        self._squeeze = None
-        self._load = (
-            load and
-            os.path.isdir(self.SUBPATHS["checkpoint_folder"].path) and
-            len(os.listdir(self.SUBPATHS["checkpoint_folder"].path)) > 0
-        )
+        # TODO: dynamically generate k-means
+        self._paths["anchors_file"] = os.path.join(config.RESOURCES_ROOT, config.WEIGHTS_FOLDER_NAME, "yolo_anchors")
 
-    # @Trainer._setup
-    def setup(self,) -> None:
-        """Set up training the SqueezeDet model by populating the configuration.
-
-        If <use_pretrained_weights> is set to `True` then the model will load
-        pretrained weights as a fallback. If <reduce_lr_on_plateau> is set to
-        `True` then the learning rate will be slowly decreased as we train if
-        we hit a plateau.
-        """
-        self._squeeze = SqueezeDetModel(self.SUBPATHS["checkpoint_folder"].path)
-
-    # @Trainer._train
-    def train(self, epochs: int = 100, nbatches_train : int = 100) -> None:
-        """Begin training the model.
-
-        Train for <epochs> epochs before automatically stopping and
-        generating the trained model.
-        """
-        tbCallBack = TensorBoard(log_dir=self.SUBPATHS['tensorboard_folder'].path, histogram_freq=0, write_graph=True, write_images=True)
-
-        squeeze = self._squeeze.model
-        squeeze.model.compile(
-            optimizer=self.get_optimizer(),
-            loss=[squeeze.loss], 
-            metrics=[squeeze.loss_without_regularization, squeeze.bbox_loss, squeeze.class_loss, squeeze.conf_loss]
-        )
-
-        squeeze.model.fit_generator(
-            self._data.generate_data(self._squeeze.config), 
-            epochs=epochs,
-            steps_per_epoch=nbatches_train, callbacks=[
-                tbCallBack
-            ]
-        )
-
-    def generate_model(self) -> Optional[SqueezeDetModel]:
+    @property
+    def model(self) -> Optional[SqueezedetModel]:
         """Generate and return the currently available prediction model.
 
         Model may be `None` if there is no currently available model.
         """
-        return SqueezeDetModel(checkpoint_path=self.SUBPATHS['checkpoint_folder'].path)
+        cfg = self._paths["config_file"]
 
-    def get_optimizer(self):
-        opt = optimizers.Adam(lr=0.001,  clipnorm=self._squeeze.config.MAX_GRAD_NORM)
-        return opt
+        model = None
+        if os.path.exists(cfg):
+            model = SqueezedetModel(cfg, self.settings)
+        return model
 
+    def get_weights_path(self) -> str:
+        """Get the path to most up-to-date weights associated with this trainer."""
+        checkpoints_dir = os.path.join(self._paths["log_folder"], "checkpoints")
+        if os.path.isdir(checkpoints_dir):
+            checkpoints = os.listdir(checkpoints_dir)
+            if checkpoints:
+                return os.path.join(
+                    checkpoints_dir,
+                    max(checkpoints, key=lambda checkpoint: float(checkpoint.split(".")[1].split("-")[1])))
+        return SqueezedetTrainer.INITIAL_WEIGHTS
 
-if __name__ == '__main__':
-    trainer = SqueezeDetTrainer('trainer', 'Bosch')
-    trainer.setup()
-    trainer.train()
+    def train(self, settings: Optional[SqueezedetSettings] = None) -> None:
+        """Begin training the model."""
+        settings = settings if settings else self._load_settings()
+        self._set_settings(settings)
+
+        from lns.squeezedet._lib.config.create_config import squeezeDet_config
+        cfg = squeezeDet_config("")
+        cfg.CLASS_NAMES = self.dataset.classes
+        cfg.CLASSES = len(cfg.CLASS_NAMES)
+        cfg.CLASS_TO_IDX = dict(zip(cfg.CLASS_NAMES, range(cfg.CLASSES)))
+        cfg.KEEP_PROB = 1 - self.settings.dropout_ratio
+        for field, setting in dataclasses.asdict(self.settings).items():
+            setattr(cfg, field.upper(), setting)
+        cfg.ANCHOR_PER_GRID = len(cfg.ANCHOR_SEED)
+        cfg.ANCHORS_WIDTH = cfg.IMAGE_WIDTH / self.settings.anchor_size
+        cfg.ANCHORS_HEIGHT = cfg.IMAGE_HEIGHT / self.settings.anchor_size
+
+        from lns.squeezedet._lib.config.create_config import save_dict
+        save_dict(cfg, self._paths["config_file"])
+
+        from lns.squeezedet._lib import train
+        train.img_file = self.data.get_images()
+        train.gt_file = self.data.get_labels()
+        train.log_dir_name = self._paths["log_folder"]
+        train.init_file = self.settings.initial_weights if self.settings.initial_weights else self.get_weights_path()
+        train.EPOCHS = self.settings.num_epochs
+        train.OPTIMIZER = self.settings.optimizer
+        train.REDUCELRONPLATEAU = self.settings.reduce_lr_on_plateau
+        train.VERBOSE = self.settings.verbose
+        train.CONFIG = self._paths["config_file"]
+
+        try:
+            train.train()
+        except KeyboardInterrupt:
+            print(f"Training interrupted")
+        else:
+            print(f"Training completed succesfully")
