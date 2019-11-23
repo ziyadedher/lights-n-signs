677bd7d1fe69d4a31b04d9cb4553021380f30c19
Jacob Kelly <jacob.kelly@autodrive.utoronto.ca>
2019-11-23 12:57:43 -0500

    fix more linting errors

--- lns/common/utils/dataset_prune_test.py
+++ lns/common/utils/dataset_prune_test.py
@@ -1,13 +1,15 @@
-import numpy as np
-import matplotlib.pyplot as plt
+"""Simple demo to test dataset pruning."""
 
 from collections import defaultdict
+
+from typing import Dict, List, Tuple
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 from lns.common import config
 from lns.common.dataset import Dataset
 from lns.common.utils.img_area import img_area
-
-from typing import Dict, List, Tuple
 
 
 def distance(dataset: Dataset) -> Dict[str, List[Tuple[int, float]]]:
@@ -30,28 +32,23 @@ def distance(dataset: Dataset) -> Dict[str, List[Tuple[int, float]]]:
 if __name__ == "__main__":
     from lns.common.preprocess import Preprocessor
     Preprocessor.init_cached_preprocessed_data()
-    thresholds = {
-        "LISA": 1.52e-8,
-        "Bosch": 7.58e-10,
-        "ScaleLights": 1.67e-5
-    }
     for dataset_name in config.POSSIBLE_DATASETS:
         if dataset_name in ["mocked", "ScaleSigns", "ScaleObjects"]:
             # TODO: ScaleSigns and ScaleObjects haven't been cached yet
             continue
         print(dataset_name)
 
-        dataset = Preprocessor.preprocess(dataset_name)
+        dataset = Preprocessor.preprocess(dataset_name).prune(config.THRESHOLDS[dataset_name])
         result = distance(dataset)
 
         # aggregate statistics over dataset by class
-        dists: Dict[int, float] = defaultdict(list)
+        dists: Dict[int, List[float]] = defaultdict(list)
         for img in result:
             for bb in result[img]:
                 dists[bb[0]].append(bb[1])
 
         class_names = dataset.classes
-        all_dists = []
+        all_dists: List[float] = []
         for class_index in dists:
             print(class_names[class_index])
             dists[class_index] = np.array(dists[class_index])
@@ -80,6 +77,3 @@ if __name__ == "__main__":
         plt.hist(np.log(all_dists))
         plt.savefig(plt_name)
         plt.clf()
-
-
-
