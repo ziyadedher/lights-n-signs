299ca61b76fe2028a23d71a18da4fc3130b93f07
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2019-11-23 11:50:03 -0500

    Slight dataset distance restructuring

--- lns/common/dataset.py
+++ lns/common/dataset.py
@@ -4,15 +4,11 @@ Contains classes for the tracking of dataset objects through our pipeline
 as well as simple utility functions for operation on the datasets.
 """
 
-from typing import Dict, List
-
 import collections
 import copy
-import os
-import struct
+from typing import Dict, List, Tuple
 
 from lns.common.structs import Object2D
-from lns.common import config
 
 _Images = List[str]
 _Classes = List[str]
@@ -98,14 +94,8 @@ class Dataset:
 
         return Dataset(self.name + name_postfix, images, classes, annotations)
 
-    def prune(self):
-        """Prune detections which are too small.
-        """
-        try:
-            threshold = config.THRESHOLDS[self.name]
-        except KeyError:
-            raise Exception("No threshold has been determined for this dataset.")
-
+    def prune(self, threshold: float) -> 'Dataset':
+        """Return a new dataset with relative annotation sizes under a given <threshold> pruned."""
         dists: Dict[float, Tuple[str, Object2D]] = collections.defaultdict(list)
 
         images = self.images
@@ -125,88 +115,7 @@ class Dataset:
                 if detection in annotations[image]:
                     annotations[image].remove(detection)
 
-
         return Dataset(self.name, images, classes, annotations)
-
-    @staticmethod
-    def _img_area(img_name: str) -> float:
-        """Determine dimensions of image stored at absolute path <img_name>."""
-        width, height = Dataset._get_image_size(img_name)
-        return width * height
-
-
-    # https://stackoverflow.com/questions/15800704/get-image-size-without-loading-image-into-memory
-    # https://raw.githubusercontent.com/scardine/image_size/master/get_image_size.py
-    class UnknownImageFormat(Exception):
-        """Exception for parsing metadata of image in unknown format."""
-
-    @staticmethod
-    def _get_image_size(file_path):
-        """Return (width, height) for a given img file content w/ no external dependencies."""
-        size = os.path.getsize(file_path)
-
-        with open(file_path, "rb") as input_file:
-            height = -1
-            width = -1
-            data = input_file.read(25)
-
-            if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
-                # GIFs
-                raw_w, raw_h = struct.unpack("<HH", data[6:10])
-                width = int(raw_w)
-                height = int(raw_h)
-            elif ((size >= 24) and data.startswith(b'\211PNG\r\n\032\n')
-                  and (data[12:16] == 'IHDR')):
-                # PNGs
-                raw_w, raw_h = struct.unpack(">LL", data[16:24])
-                width = int(raw_w)
-                height = int(raw_h)
-            elif (size >= 16) and data.startswith(b'\211PNG\r\n\032\n'):
-                # older PNGs?
-                raw_w, raw_h = struct.unpack(">LL", data[8:16])
-                width = int(raw_w)
-                height = int(raw_h)
-            elif (size >= 2) and data.startswith(b'\377\330'):
-                # JPEG
-                width, height = Dataset._process_jpeg(input_file)
-            else:
-                raise UnknownImageFormat(
-                    "Sorry, don't know how to get information from this file."
-                )
-
-        return width, height
-
-    @staticmethod
-    def _process_jpeg(input_file):
-        """Process metadata of JPEG."""
-        msg = " raised while trying to decode as JPEG."
-        input_file.seek(0)
-        input_file.read(2)
-        binary = input_file.read(1)
-        try:
-            while binary and ord(binary) != 0xDA:
-                while ord(binary) != 0xFF:
-                    binary = input_file.read(1)
-                while ord(binary) == 0xFF:
-                    binary = input_file.read(1)
-                if (ord(binary) >= 0xC0 and ord(binary) <= 0xC3):
-                    input_file.read(3)
-                    raw_h, raw_w = struct.unpack(">HH", input_file.read(4))
-                    break
-                else:
-                    input_file.read(int(struct.unpack(">H", input_file.read(2))[0]) - 2)
-                binary = input_file.read(1)
-            width = int(raw_w)
-            height = int(raw_h)
-        except struct.error:
-            raise UnknownImageFormat("StructError" + msg)
-        except ValueError:
-            raise UnknownImageFormat("ValueError" + msg)
-        except Exception as ex:
-            raise UnknownImageFormat(ex.__class__.__name__ + msg)
-
-        return width, height
-
 
     def __add__(self, other: 'Dataset') -> 'Dataset':
         """Magic method for adding two preprocessing data objects."""
