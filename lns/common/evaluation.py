bbbdeb3863d6daeaf524199946cf87863e060f89
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2020-02-01 14:23:29 -0500

    Include profiling

--- lns/common/evaluation.py
+++ lns/common/evaluation.py
@@ -1,4 +1,6 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Callable
+
+import time
 
 from tqdm import tqdm  # type: ignore
 import numpy as np  # type: ignore
@@ -14,15 +16,20 @@ Metrics = List[Tuple[float, float, float]]
 
 
 def confusion(model: Model, dataset: Union[str, Dataset],
-              iou_threshold: float = 0.25) -> ConfusionMatrix:
+              class_mapping: Callable[[int], int] = lambda x: x,
+              iou_threshold: float = 0.25) -> Tuple[ConfusionMatrix, float]:
     if isinstance(dataset, str):
         dataset = Preprocessor.preprocess(dataset)
 
     num_classes = len(dataset.classes)
     mat = np.zeros((num_classes + 1, num_classes + 1), dtype=np.int32)
 
+    total_time = 0.
     for img_path, labels in tqdm(dataset.annotations.items()):
+        start_time = time.time()
         preds = model.predict_path(img_path)
+        total_time += time.time() - start_time
+
         label_detected = np.zeros((len(labels)), dtype=np.bool)
         pred_associated = np.zeros((len(preds)), dtype=np.bool)
 
@@ -31,9 +38,12 @@ def confusion(model: Model, dataset: Union[str, Dataset],
                 if pred_associated[j]:
                     continue
 
+                label_class = label.class_index
+                pred_class = class_mapping(pred.class_index)
+
                 if iou(pred.bounds, label.bounds) >= iou_threshold:
-                    mat[label.class_index][pred.class_index] += 1
-                    if label.class_index == pred.class_index:
+                    mat[label_class][pred_class] += 1
+                    if label_class == pred_class:
                         label_detected[i] = True
                         pred_associated[j] = True
 
@@ -44,7 +54,7 @@ def confusion(model: Model, dataset: Union[str, Dataset],
             if not pred_associated[j]:
                 mat[-1][pred.class_index] += 1
 
-    return mat
+    return mat, (total_time / len(dataset.images))
 
 
 def metrics(mat: ConfusionMatrix) -> Metrics:
