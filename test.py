b177cbf90ceec32b3e402d6a963bb8ada3304b15
sherrychen127 <sherrychen127@gmail.com>
2020-02-01 14:34:12 -0500

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-"""Test the anchor generation and compare to YOLO."""
-from lns.common.preprocess import Preprocessor
-dataset = Preprocessor.preprocess('ScaleLights')
-anchors = dataset.generate_anchors(9)
-
-
-# print(anchors)
