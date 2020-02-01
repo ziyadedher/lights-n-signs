1d88211882cd8dda6ed66e006cfa98c09180e303
Ziyad Edher <ziyad.edher@autodrive.utoronto.ca>
2020-02-01 15:36:06 -0500

    Write images locally

--- lns/common/evaluation.py
+++ lns/common/evaluation.py
@@ -62,7 +62,8 @@ def confusion(model: Model, dataset: Union[str, Dataset],
         img = cv2.imread(img_path)
         visualization.draw_labels(img, labels, (255, 255, 255), 2)
         visualization.draw_labels(img, preds, (0, 0, 0), 2)
-        cv2.imwrite(f"imgs/{img_path}", img)
+        cv2.imwrite(f"test.png", img)
+        input()
 
         for i, label in enumerate(labels):
             for j, pred in enumerate(preds):
