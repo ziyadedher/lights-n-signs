70f86518ef8fb9647c24a9fcddc5354ef18b720b
mchanchee <matthieuchanchee@gmail.com>
2021-01-31 13:03:44 -0500

    merged class name in evaluation

--- scripts/evaluate_haar.py
+++ scripts/evaluate_haar.py
@@ -1,6 +1,6 @@
 from lns.haar.eval import evaluate
 
-results = evaluate(data_path='/mnt/ssd1/lns/resources/processed/haar/Y4Signs/annotations/No_Left_Turn_Text_positive',
+results = evaluate(data_path='/mnt/ssd1/lns/resources/processed/haar/Y4Signs/annotations/nrt_nlt_sym_positive',
                    model_path='/mnt/ssd1/lns/resources/trainers/haar/matthieu_haar_y4signs_1/cascade/cascade.xml')
 
 tp, fp, precision, recall, f1_score = results
