fdd38429a76aa96668f7ea06c34d41d61c36b083
Manav Shah <manav.shah@autodrive.utoronto.ca>
2021-03-09 20:56:49 -0500

    Added automatic test dataset preprocessing if it doesn't exist

--- scripts/train_haar_updated.py
+++ scripts/train_haar_updated.py
@@ -1,7 +1,9 @@
+import os
 from lns.common.preprocess import Preprocessor
 from lns.haar.train import HaarTrainer
 from lns.haar.eval import evaluate
 from lns.haar.settings import HaarSettings
+from lns.haar.process import HaarProcessor
 
 
 
@@ -38,7 +40,7 @@ trainer = HaarTrainer(name=model_name,
                         class_index = new_class_index,
                         dataset=dataset_y4signs, 
                         load=False, # Training from scratch
-                        forcePreprocessing = FORCE_PREPROCESSING) 
+                        forcePreprocessing = FORCE_PREPROCESSING) # True means preprocessing from scratch
 
 trainer.setup()
 trainer.train()
@@ -49,6 +51,25 @@ print("Evaluating model for: " + str(dataset_y4signs.classes[HaarSettings.class_
 
 # IMPORTANT: must preprocess the test folder before running this script. This name should exist in '/home/od/.lns-training/resources/processed/haar/'
 eval_preprocessed_folder = 'Y4Signs_filtered_1036_584_test_split'
+
+processed_path_for_eval = '/home/od/.lns-training/resources/processed/haar/{0}'.format(eval_preprocessed_folder)
+
+if not os.path.exists(eval_preprocessed_folder):
+  print("did not find preprocessed test dataset, preprocessing from scratch")
+  # TODO: preprocess the evaluation dataset
+  dataset_y4signs_eval = Preprocessor.preprocess('Y4Signs_filtered_1036_584_test_split', force=True) # force = True will give stats
+  dataset_y4signs_eval = dataset_y4signs_eval.merge_classes({
+  "nrt_nlt_text": ['No_Right_Turn_Text', 'No_Left_Turn_Text'],
+  "nrt_nlt_sym": ['No_Right_Turn_Sym', 'No_Left_Turn_Sym']
+  })
+  _ = HaarProcessor.process(dataset_y4signs_eval, force=True)
+
+  print("Preprocessing complete! Evaluating...")
+  
+
+
+
+
 
 # evaluate model after training and save visualisations and numeric data
 results = evaluate(data_path='/home/od/.lns-training/resources/processed/haar/{0}/annotations/{1}_positive'.format(eval_preprocessed_folder, class_to_classify),
