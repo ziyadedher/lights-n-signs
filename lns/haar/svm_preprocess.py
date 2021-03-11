9fb4b0055a6155fba78b72337e8e0bcabbe4348c
helen9975 <helen1999@live.ca>
2021-03-10 21:34:02 -0500

    Fix cv2 error

--- lns/haar/svm_preprocess.py
+++ lns/haar/svm_preprocess.py
@@ -33,10 +33,15 @@ class SVMProcessor:
 
         print("Creating crops...")
         with tqdm(desc="Processing", total=len(self.dataset.annotations.keys()), miniters=1) as tqdm_bar:
+            need_print = 1
             for image_path, labels in self.dataset.annotations.items():
                 tqdm_bar.update()
-                colour_image = cv.imread(image_path)
+                
+                #gray_image = cv.imread(image_path,0)
+                
                 gray_image = np.array(cv.cvtColor(colour_image, cv.COLOR_BGR2GRAY)) # load gray image in numpy array
+                
+                
                 for label in labels:
                     if label.class_index in self.splits:
                         xmin = label.bounds.left
@@ -44,25 +49,38 @@ class SVMProcessor:
                         ymin = label.bounds.top
                         ymax = label.bounds.bottom
                         crop = gray_image[ymin:ymax, xmin:xmax]
-                        self.splits[label.class_index].append(crop)
-        # self.save_np_arrays()
+                        if need_print:
+                            print('stage 1',crop)
+                        img = cv.resize(crop,(32, 32))
+                        img = cv.equalizeHist(img)
+                        if need_print:
+                            print('stage 2', img)
+                            need_print = False
+                        self.splits[label.class_index].append(img)
+        self.save_np_arrays()
 
     
     def save_np_arrays(self, force: bool = False):
         print("Saving pre-processed crops...")
         for class_a, class_b in self.compare:
-            zeros = np.array(self.splits[class_a], dtype=object) # all crops of class_a
-            ones = np.array(self.splits[class_b], dtype=object) # all crops of class_b
+            for i in range(len(self.splits[class_a])):
+                self.splits[class_a][i] = np.array(self.splits[class_a][i], dtype=np.float32)
+            zeros = np.array(self.splits[class_a]) # all crops of class_a
+            for i in range(len(self.splits[class_b])):
+                self.splits[class_b][i] = np.array(self.splits[class_b][i], dtype=np.float32)
+            ones = np.array(np.array(self.splits[class_b])) # all crops of class_b
             data_x = np.concatenate((zeros, ones), axis=0)
             labels = np.concatenate((np.zeros(len(zeros)), np.ones(len(ones)))) # class_a corresponds to 0 and so on
+            labels = np.array(labels, dtype=np.int32)
             assert len(zeros) + len(ones) == len(labels)
             subfolder = os.path.join(self.path, str(self.dataset.classes[class_a]) + '_' + self.dataset.classes[class_b])
             if not os.path.exists(subfolder):
                 os.makedirs(subfolder)
             data_path = os.path.join(subfolder, "data.npy")
             labels_path = os.path.join(subfolder, "labels.npy")
+            data_x = np.reshape(data_x,(data_x.shape[0],data_x.shape[1]*data_x.shape[2]))
             np.save(data_path, data_x)
-            np.save(labels_path, np.array(labels, dtype=np.int64))
+            np.save(labels_path, np.array(labels, dtype=np.int32))
         
         print("Save complete.")
         print("Saved at: " + self.path)
--- scripts/train_svm.py
+++ scripts/train_svm.py
@@ -3,19 +3,19 @@ from lns.haar.svm_preprocess import SVMProcessor
 from lns.haar.svm_train import SVMTrainer
 from lns.common.preprocess import Preprocessor
 
-model_path = '~/.lns-training/resources/trainers/haar'
+model_path = '~/home/lns/.lns-training/resources/trainers/haar'
 processed_path_train = '~/.lns-training/resources/processed/haar/y4Signs_train_svm'
 processed_path_test = '~/.lns-training/resources/processed/haar/y4Signs_test_svm'
 lrt_data_path = 'No_Right_Turn_Sym_No_Left_Turn_Sym/data.npy'
 lrt_labels = 'No_Right_Turn_Sym_No_Left_Turn_Sym/labels.npy'
 lrt_text_data_path = 'No_Right_Turn_Text_No_Left_Turn_Text/data.npy'
 lrt_text_labels = 'No_Right_Turn_Text_No_Left_Turn_Text/labels.npy'
-PROCESS_TRAIN, PROCESS_TEST = True, True
-TRAIN_SYM, TRAIN_TEXT = True, True
-TEST_SYM, TEST_TEXT = True, True
+PROCESS_TRAIN, PROCESS_TEST = False, False
+TRAIN_SYM, TRAIN_TEXT = True, False
+TEST_SYM, TEST_TEXT = False, False
 
 if PROCESS_TRAIN:
-    dataset = Preprocessor.preprocess('Y4Signs_1036_584_train', force=True)
+    dataset = Preprocessor.preprocess('Y4Signs_filtered_1036_584_train_split', force=True)
 
     to_classify = [(0, 2), (1, 3)]  # class indices to differentiate between
     # (0, 2) means that we are classifying between no_Right_Turn_Sym and no_Left_Turn_Sym
@@ -31,7 +31,7 @@ if TRAIN_SYM:
     print("Training No_Right_Turn_Sym_No_Left_Turn_Sym")
     trainer = SVMTrainer(os.path.join(processed_path_train, lrt_data_path),
                          os.path.join(processed_path_train, lrt_labels),
-                         os.path.join(model_path, 'heddy_lrt_sign'))
+                         os.path.join(model_path, 'helen_lrt_sign'))
     trainer.setup()
     trainer.train()
     print("Done")
@@ -39,7 +39,7 @@ if TRAIN_TEXT:
     print("Training No_Right_Turn_Text_No_Left_Turn_Text")
     trainer = SVMTrainer(os.path.join(processed_path_train, lrt_text_data_path),
                          os.path.join(processed_path_train, lrt_text_labels),
-                         os.path.join(model_path, 'heddy_lrt_text'))
+                         os.path.join(model_path, 'helen_lrt_text'))
     trainer.setup()
     trainer.train()
     print("Done")
@@ -51,12 +51,12 @@ if PROCESS_TEST:
     processor.save_np_arrays()
 
 if TEST_SYM:
-    test = SVMClassifer(os.path.join(model_path, 'heddy_lrt_sign'))
+    test = SVMClassifer(os.path.join(model_path, 'helen_lrt_sign'))
     print("Evaluating NoRight Turn Sym NoLeft Turn Sym")
     test.eval(os.path.join(processed_path_test, lrt_data_path),
               os.path.join(processed_path_test, lrt_labels))
 if TEST_TEXT:
-    test = SVMClassifer(os.path.join(model_path, 'heddy_lrt_text'))
+    test = SVMClassifer(os.path.join(model_path, 'helen_lrt_text'))
     print("Evaluating NoRight Turn Text NoLeft Turn Text")
     test.eval(os.path.join(processed_path_test, lrt_text_data_path),
               os.path.join(processed_path_test, lrt_text_labels))
