89dcb9071434e410ffc60972a27d880cea881fae
tiffanyyk <tiffanyyk.yau@mail.utoronto.ca>
2021-04-24 17:51:54 -0400

    change for SVM size, heddys multiclass test code

--- scripts/train_multiclass_svm.py
+++ scripts/train_multiclass_svm.py
@@ -1,12 +1,14 @@
 import os
-from lns.haar.svm_classifier import SVMClassifier
+from lns.haar.svm_classifier import SVMClassifier, f1_score
 from lns.haar.svm_preprocess_one_v_all import SVMProcessor
 from lns.haar.svm_train import SVMTrainer
 from lns.common.preprocess import Preprocessor
+import cv2 as cv
+import numpy as np
 
 MODEL_OUTPUT_PATH = '/home/od/.lns-training/resources/trainers/haar'
-PATH_TRAIN = '/home/od/.lns-training/resources/processed/haar/y4Signs_train_text_svm'
-PATH_TEST = '/home/od/.lns-training/resources/processed/haar/y4Signs_test_text_svm'
+PATH_TRAIN = '/home/od/.lns-training/resources/processed/haar/y4Signs_train_text_svm_48'
+PATH_TEST = '/home/od/.lns-training/resources/processed/haar/y4Signs_test_text_svm_48'
 
 def get_classes_to_classify(y4_signs_folder='Y4Signs_filtered_1036_584_train_split'):
     """
@@ -52,7 +54,7 @@ def evaluate_svm_models(test_data, labels, model_name, processed_data_path=PATH_
     """
     path = os.path.join(model_path, model_name)
     print(f"\nEvaluating {model_name} SVM at {path}")
-    test = SVMClassifier(path)
+    test = SVMClassifier(path, input_size=(48, 48))
     test.eval(os.path.join(processed_data_path, test_data),
               os.path.join(processed_data_path, labels))
 
@@ -63,12 +65,57 @@ def _data_path(i):
     return [
         os.path.join(signs[i], "data.npy"),
         os.path.join(signs[i], "labels.npy"),
-        "heddy_text_{}_vs_rest".format(models[i])
+        "tiffany_text_{}_vs_rest".format(models[i])
     ]
 
 
+def _test_joint(input_size=(48, 48)):
+    signs = ["No_Right_Turn_Text", "No_Left_Turn_Text", "Right Turn Only (words)", "Left Turn Only (words)"]
+    models = ["nrt", "nlt", "rto", "lto"]
+
+    tp, fp, total = 0, 0, 0
+
+    # i = the current ground truth
+    for i in range(4):
+        val_path = os.path.join(PATH_TEST, signs[i], "data.npy")
+        labels_path = os.path.join(PATH_TEST, signs[i], "labels.npy")
+        val_data = np.load(val_path, allow_pickle=True)
+        val_data = np.float32(val_data).reshape((-1, input_size[0] * input_size[1]))
+        val_labels = np.load(labels_path, allow_pickle=True)
+        val_labels = np.int32(val_labels).reshape((val_labels.shape[0],1))
+
+        res = []
+        for j in range(4):
+            model_path = os.path.join(MODEL_OUTPUT_PATH, "tiffany_text_{}_vs_rest".format(models[j]))
+
+            trained_model = cv.ml.SVM_load(model_path + "/svm.xml")
+            predicted_results = trained_model.predict(val_data)[1]
+            res.append(predicted_results)
+
+        for v in range(val_data.shape[0]):  # loop through all val samples
+            if val_labels[v] == 0:  # if the label is 0 which means is a ground truth positive sample
+    
+                results_to_verify = [res[0][v], res[1][v], res[2][v], res[3][v]]  # for this sample, the values outputted by each SVM
+                # print(results_to_verify)  # debug note: res[0] mostly giving 1, res[1] mostly 0 but some 1, res[2] / res[3] are basically all 0 from what I see (3 is worst)
+                predicted = np.argmin(results_to_verify)  # 0 is positive so take minimum confidence
+                if predicted == i:  # if predicted is the ground truth
+                    # if i == 2:
+                    #     cv.imwrite("test/label-" + str(i) + "-" + str(v) + "true" + str(predicted) + ".jpg", np.resize(val_data[v], (32, 32)))
+                    tp += 1
+                else:
+                    fp += 1
+                # total += 1
+
+
+    precision = float(tp) / float(fp + tp)
+    recall = float(tp) / float(fp + tp)
+
+    print(total)
+    print("TP: {}\FP: {}\Precision: {:.2f}\Recall: {:.2f}\nF1 score: {:.2f}".format(tp, fp, precision, recall, f1_score(precision, recall)))
+
+
 PROCESS_TRAIN, PROCESS_TEST = False, False  # DO NOT TURN ON, only if data re-processing is required
-TRAIN, TEST = True, True
+TRAIN, TEST, TEST_JOINT = True, True, True
 
 if PROCESS_TRAIN:
     dataset, to_classify = get_classes_to_classify()
@@ -117,3 +164,7 @@ if TEST:
     # Test: Left Turn Only Text vs. rest
     files_path = _data_path(3)
     evaluate_svm_models(files_path[0], files_path[1], files_path[2])
+
+if TEST_JOINT:
+    _test_joint()
+
