6af3b56f0107110a6481e0ec62fc374e7cace329
cheongb002 <cheongb002@gmail.com>
2021-05-11 22:34:40 -0400

    working to run on Squeezedet model

--- scripts/video_visualize_lights.py
+++ scripts/video_visualize_lights.py
@@ -11,6 +11,7 @@ from lns.common.dataset import Dataset
 from lns.common.visualization import *
 
 from lns.yolo.train import YoloTrainer
+from lns.squeezedet.train import SqueezedetTrainer
 #from lns.yolo.settings import YoloSettings
 
 fps = 5
@@ -21,18 +22,18 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "2" #choose which GPU to use, I don't actua
 
 # Video file structure is as follows: ../ped_dummy_vides_2021/timestamped folder/videos
 # Each timestamped folder contains 4 videos, 1 from each camera on Zeus.
-frames_folder_parent = '/mnt/ssd2/od/datasets/20210418/frames' #'/mnt/ssd2/od/datasets/lights_vids/frames' 
+frames_folder_parent = '/home/od/files/datasets/20210418/frames' #'/mnt/ssd2/od/datasets/lights_vids/frames' 
 #"/home/od/files/datasets/ped_dummy_vids_2021/frames"
-labelled_videos_parent = "/home/lns/helen/lights-n-signs-training/labelled_videos" 
+labelled_videos_parent = "/home/od/files/datasets/20210418/labelled_videos" 
 # "/home/od/files/datasets/ped_dummy_vids_2021/labelled_videos"
 
-trainer_name = 'matthieu_darknet53_416_3' #'matthieu_darknet53_256_1'#'matthieu_darknet53_384_1' #'matthieu_darknet53_416_4'#'matthieu_darknet53_416_3' #'new_dataset_ac_small_3' #'new_dataset_ac_medium_1' #'smaller_800x640_1' #'new_dataset_ac_small_3' #'new_dataset_ac_1' # "yolo_ped_mbd_trial_29"
-trainer = YoloTrainer(trainer_name)
+trainer_name = 'helen_squeezedet_1248_384_1' #'matthieu_darknet53_256_1'#'matthieu_darknet53_384_1' #'matthieu_darknet53_416_4'#'matthieu_darknet53_416_3' #'new_dataset_ac_small_3' #'new_dataset_ac_medium_1' #'smaller_800x640_1' #'new_dataset_ac_small_3' #'new_dataset_ac_1' # "yolo_ped_mbd_trial_29"
+trainer = SqueezedetTrainer(trainer_name)
 model = trainer.model
 
 folder_of_interest = 'traffic_control'
 
-irrelevant_cams = ['blackfly3','blackfly4']
+#irrelevant_cams = ['blackfly3','blackfly4']
 
 crop = [0.25,0.75,0.16,0.75] #[0,1,0.21,0.58]#[0.25,0.75,0.21,0.6] #[0.25,0.75,0.16,0.55] #[0.25,0.75,0.31,0.7] #[0.25,0.75,0.175,0.5]
 
